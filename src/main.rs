e53884214edb21354ba9d28efe72482874fbb6df
Timothy Lee <u6018802@anu.edu.au>
2019-02-17 15:44:38 +1100

    base crates added

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,18 @@
 // extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
 // extern crate panic_itm; // logs messages over ITM; requires ITM support
 extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
+extern crate stm32f1;
+extern crate stm32f1xx_hal;
+extern crate rtfm;
 
 use cortex_m::asm;
 use cortex_m_rt::entry;
+// use cortex_m_semihosting::{hprintln};
+// use stm32f1::stm32f103::{Peripherals};
+// use stm32f1xx_hal::prelude::*;
+// use stm32f1xx_hal::delay::Delay;
+// use rtfm::app; 
+
 
 #[entry]
 fn main() -> ! {
