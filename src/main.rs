bfd98b121122be9a503f8efd16125dc382afc356
Harrison Turton <harrisonturton@gmail.com>
2019-02-19 13:54:02 +1100

    Working rtfm on black pill

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,28 @@
 #![no_std]
 #![no_main]
 
-// pick a panicking behavior
-// extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// extern crate panic_itm; // logs messages over ITM; requires ITM support
-extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
-extern crate stm32f1;
 extern crate stm32f1xx_hal;
+extern crate panic_semihosting; // Log panic errors to stderr
 extern crate rtfm;
 
-use cortex_m::{asm, Peripherals as core_peripherals};
-use cortex_m_rt::entry;
-// use cortex_m_semihosting::{hprintln};
-use stm32f1::stm32f103::{Peripherals as board_peripherals};
-// use stm32f1xx_hal::prelude::*;
-// use stm32f1xx_hal::delay::Delay;
-// use rtfm::app; 
+use cortex_m_semihosting::hprintln;
+use stm32f1xx_hal::stm32;
+use rtfm::app;
 
+#[app(device = stm32f1xx_hal::stm32)]
+const APP: () = {
 
-#[entry]
-fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
-
-    loop {
-        // your code goes here
+    #[init]    
+    unsafe fn init() {
+        hprintln!("Starting!!").unwrap();
     }
-}
+
+    #[idle]
+    unsafe fn idle() -> ! {
+        hprintln!("Idle!").unwrap();
+        loop {
+            hprintln!("Hello World!").unwrap();
+        }
+    }
+
+};
