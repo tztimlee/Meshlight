5cae8879dbf0f7cb60012314c416b28ddeaa0f8c
Harrison Turton <harrisonturton@gmail.com>
2019-02-21 17:57:18 +1100

    Commented unused imports

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,35 @@
 
-#![feature(alloc)]
-#![feature(alloc_error_handler)]
+//#![feature(alloc)]
+//#![feature(alloc_error_handler)]
 #![no_std]
 #![no_main]
 
+//mod router;
+
 // alloc
-extern crate alloc;
+//extern crate alloc;
 extern crate stm32f1;
 extern crate stm32f1xx_hal;
 extern crate rtfm;
 extern crate panic_semihosting; // Log panic errors to stderr
 
-use alloc_cortex_m::CortexMHeap;
-use cortex_m::asm;
-use core::alloc::Layout;
-use cortex_m_rt::entry;
+//use alloc_cortex_m::CortexMHeap;
+//use cortex_m::asm;
+//use core::alloc::Layout;
 
-use stm32f1::stm32f103;
-use stm32f1::stm32f103::Interrupt;
-
-mod router;
+//use stm32f1::stm32f103::Interrupt;
 
 use cortex_m_semihosting::hprintln;
-use stm32f1xx_hal::stm32;
 use rtfm::app;
 
-use stm32f1xx_hal::prelude::*;
+//use stm32f1xx_hal::prelude::*;
 
-use self::alloc::vec;
+//use self::alloc::vec;
 
-#[global_allocator]
-static ALLOCATOR: CortexMHeap = CortexMHeap::empty();
-
-const HEAP_SIZE: usize = 10240;
+//#[global_allocator]
+//static ALLOCATOR: CortexMHeap = CortexMHeap::empty();
+//
+//const HEAP_SIZE: usize = 10240;
 
 #[app(device = stm32f1xx_hal::stm32)]
 const APP: () = {
@@ -62,7 +59,7 @@ const APP: () = {
         gpiob.brr.write(|w| w.br12().set_bit());
 
         loop {
-            hprintln!("looping");
+            hprintln!("looping").unwrap();
 
             // Blink the LEDs!
             gpiob.bsrr.write(|w| w.bs12().set_bit());
@@ -75,8 +72,8 @@ const APP: () = {
 
 // When an Out Of Memory condition occurs, trigger a breakpoint (for debugging)
 // and loop indefinitely.  #[alloc_error_handler]
-#[alloc_error_handler]
+/*#[alloc_error_handler]
 fn alloc_error(_layout: Layout) -> ! {
     asm::bkpt();
     loop {}
-}
+}*/
