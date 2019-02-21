6aabe8626ecfc84431f69cf13ee1e28f8fc19f07
Harrison Turton <harrisonturton@gmail.com>
2019-02-21 15:41:31 +1100

    Clean up router

--- src/router.rs
+++ src/router.rs
@@ -1,102 +1,102 @@
 
 extern crate alloc;
 use self::alloc::collections::{BTreeMap, VecDeque};
-
-type Cost = u32;
-type PinID = u32;
-type NodeID = u32;
-
-type RoutingTable = BTreeMap<NodeID, RoutingTableEntry>;
-
-enum Message {
-    DistanceVector(NodeID, DistanceVector),
-    Heartbeat(NodeID, NodeID),
-    SendMsg(NodeID, u32)
-}
-
 use cortex_m_semihosting::hprintln;
+use self::alloc::vec;
+
+type NodeID = u8;
+type PinID = u8;
+type Cost = u8;
+
+// RoutingTable associates every known node with its
+// cost (number of network hops to reach), and the pin
+// where the message is to be sent initially.
+type RoutingTable = BTreeMap<NodeID, RoutingTableEntry>;
 
 struct RoutingTableEntry {
     cost: Cost,
-    neighbour_id: PinID
+    link_id: PinID
 }
 
-// DistanceVector is a summary of the RoutingTable
+// DitsanceVector is a summary of the routing table, without
+// node-specific information (like what pins to send data on)
 type DistanceVector = BTreeMap<NodeID, Cost>;
 
+// Message is the data passed along to a specific
+// node. It is routed depending on the given NodeID.
+type Message = (NodeID, MessageBody);
+
+// MessageBody is the actual information given to the
+// destination node.
+enum MessageBody {
+    DistanceVector(DistanceVector),
+}
+
+// Router is the routing instance for this node
 struct Router {
     id: NodeID,
     routing_table: RoutingTable,
-    dist_vec: DistanceVector,
 }
 
 impl Router {
-    pub fn new(node_id: NodeID) -> Router {
+    pub fn new(id: NodeID) -> Router {
         Router{
-            id: node_id,
-            routing_table: RoutingTable::new(),
-            dist_vec: DistanceVector::new(),
-        }
-    }
-    pub fn add_connection(&mut self, node_id: NodeID, neighbour_id: NodeID, cost: Cost) {
-        self.routing_table.insert(node_id, RoutingTableEntry{
-            neighbour_id: neighbour_id,
-            cost: cost
-        });
-        self.dist_vec.insert(node_id, cost);
+            id: id,
+            routing_table: RoutingTable::new()
+        } 
     }
     pub fn broadcast_dist_vec(&self) {
-        for (node_id, cost) in &self.dist_vec {
-            send(*node_id, Message::DistanceVector(*node_id, self.dist_vec.clone()));
+        let dist_vec = self.get_distance_vec();
+        for (node_id, node) in &self.routing_table {
+            send(*node_id, MessageBody::DistanceVector(dist_vec.clone()));
+        } 
+    }
+    pub fn receive_msg(&mut self, pin_id: PinID, msg: Message) {
+        let (node_id, msg_body) = msg;
+        // Pass message along if not meant for us
+        if node_id != self.id {
+            send(node_id, msg_body);
+            return;
+        }
+        match msg_body {
+            MessageBody::DistanceVector(dist_vec) => {
+                self.receive_dist_vec(pin_id, dist_vec);
+            },
         }
     }
-    pub fn receive_msg(&mut self, link_id: NodeID, message: Message) {
-        match message {
-            Message::DistanceVector(dest_id, dist_vec) => {
-                if dest_id != self.id {
-                    send(dest_id, Message::DistanceVector(dest_id, dist_vec));
-                } else {
-                    self.receive_dist_vec(link_id, dist_vec);
-                }
-            },
-            Message::Heartbeat(dest_id, from_id) => {
-                if dest_id != self.id {
-                    send(dest_id, message);
-                } else {
-                    self.receive_heartbeat(from_id);
-                }
-            },
-            Message::SendMsg(dest_id, msg) => {
-                if dest_id == self.id {
-                    hprintln!("Got message!"); 
-                } else {
-                    send(dest_id, message);
-                }
-            }
-        }    
-    }
-    pub fn receive_heartbeat(&self, from_id: NodeID) {
-        hprintln!("Got heartbeat from node {:?}", from_id).unwrap();
-    }
-    pub fn receive_dist_vec(&mut self, link_id: NodeID, dist_vec: DistanceVector) {
+    pub fn receive_dist_vec(&mut self, pin_id: PinID, dist_vec: DistanceVector) {
         for (node_id, cost) in dist_vec {
-            match self.dist_vec.get(&node_id) {
+            match self.routing_table.get(&node_id) {
                 // If seen route before, check if better
-                Some(current_cost) => {
+                Some(node) => {
                     let new_cost = cost + 1;
-                    if new_cost < *current_cost {
-                        self.add_connection(node_id, link_id, new_cost);
+                    if new_cost < node.cost {
+                        self.add_connection(node_id, pin_id, new_cost);
                     }
                 },
-                // If new route, then save
+                // If new route then save
                 None => {
-                    self.add_connection(node_id, link_id, cost);
+                    self.add_connection(node_id, pin_id, cost) 
                 }
             }
+        }  
+    }
+    pub fn add_connection(&mut self, node_id: NodeID, link_id: PinID, cost: Cost) {
+        self.routing_table.insert(node_id, RoutingTableEntry{
+            cost: cost,
+            link_id: link_id
+        });
+    }
+    pub fn get_distance_vec(&self) -> DistanceVector {
+        let mut dist_vec = DistanceVector::new();
+        for (node_id, node) in &self.routing_table {
+            dist_vec.insert(*node_id, node.cost);
         }
+        dist_vec
     }
 }
 
-fn send(node_id: NodeID, msg: Message) {
-    hprintln!("Sending message!").unwrap();
+fn send(node_id: NodeID, msgBody: MessageBody) {
+    // do something with (node_id, msg_body) 
+    hprintln!("Sending message...").unwrap();
 }
